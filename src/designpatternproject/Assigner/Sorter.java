8908f6619aa3fc2a2514ce774776313673f3ba5f
Nathan-G1 <nathygech111@gmail.com>
2019-02-22 21:34:49 -1000

    added dorm number assigner

---
+++ src/designpatternproject/Assigner/Sorter.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Date;
+//this class basically contains the main student assigner/ordering algorithm to be called anywhere
+//the view needs the dorm number of the specific students
+    /*
+    * And considers all the dorms are available with 8 spaces meaning for 8 students.
+    * number of available dorms to be assigned is determined by the proctor but should
+    * always be greater than (number of students / 8)
+    *
+    * */
+
+public class Sorter extends Student{
+
+    static Integer NUMBER_OF_AVAILABLE_DORMITORY = 120;
+    static Integer NUMBER_OF_STUDENTS_IN_A_DORM = 8;
+
+    private int DormNumber;
+
+    //constructor with a new property to the student object
+    public Sorter(String name, String field, Date year, int DormNumber){
+        super(name,field,year);
+        this.DormNumber = DormNumber;
+    }
+
+
+    ArrayList<Student> students = new ArrayList<Student>();
+    public int dormAssiner(String name, String field){
+        int dormNumber = 0;
+        int counter = 0;
+        int stable = 0;
+
+        Student ar[][] = new Student[NUMBER_OF_AVAILABLE_DORMITORY][NUMBER_OF_STUDENTS_IN_A_DORM];
+        int f = 0;
+        for (int i = 0; i < 120; i++){
+            for (int j = 0; j < NUMBER_OF_STUDENTS_IN_A_DORM; j ++){
+                if ((students.get(i).getField()).equals("Chemical")){
+                    ar[i][j] = students.get(i);
+                }else if((students.get(i).getField()).equals("Software")) {
+                    ar[i][j] = students.get(i);
+                }else if((students.get(i).getField()).equals("Civil")){
+                    stable = NUMBER_OF_AVAILABLE_DORMITORY - counter;
+                }else if((students.get(i).getField()).equals("Biomedical")){
+                    ar[i][j] = students.get(i);
+                }else if((students.get(i).getField()).equals("Mechanical")){
+                    ar[i][j] = students.get(i);
+                }else if((students.get(i).getField()).equals("IT")){
+                    ar[i][j] = students.get(i);
+                }
+            }
+        }
+
+        for(int i = 0; i < students.size(); ++i){
+                stable = NUMBER_OF_AVAILABLE_DORMITORY - counter;
+
+        }
+
+        return 0;
+    }
+
+}
